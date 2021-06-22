0fb4ef6d8d5b2bb24659a21f6912c0ba5ac01523
Jerónimo Santa María Illanes <jero@MacBook-Pro-de-Jeronimo.local>
2021-06-22 15:27:51 -0400

    base to hack with the basic structure

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './views/home';
+import NotFound from './views/notfound';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <div className="App">
-      <div>App</div>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ front/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = props => {
+    return(
+        <div className="footer">
+            <h1>Diseñado y Programado por JJWebStudio</h1>
+        </div>
+    );
+}
+
+export default Footer;
