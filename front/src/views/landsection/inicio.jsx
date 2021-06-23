05cf4e0ef2edcae962fd3e0827d5d2dce202051f
Jerónimo Santa María Illanes <jero@MacBook-Pro-de-Jeronimo.local>
2021-06-23 16:43:11 -0400

    some changes in inicio

--- front/src/views/landsection/inicio.jsx
+++ front/src/views/landsection/inicio.jsx
@@ -15,20 +15,16 @@ const Inicio = props => {
             </div>
             <div className="middle d-flex justify-content-center no-gutters">
                 <div className="col-6 ml-5">
-                    <div className="some d-flex no-gutters">
+                    <div className="some d-flex my-2 no-gutters">
                         <img src={IsoPP} alt="isopp" className="isopp" />
-                        <h1>COBRANZA JUDICIAL</h1>
+                        <h1>COBRANZA JUDICIAL<br/>de facturas, cheques, pagarés, etc</h1>
                     </div>
                     <div className="restotexto ml-5">
-                        <h5>de facturas, cheques, pagarés, etc</h5>
-                        <p>PROTEGEMOS TU PYME Y TE AYDUAMOS EN LOS</p>
-                        <p>PROCESOS DE COBRANZA</p>
-                        <p>SI NO RECUPERAS, ¡NO PAGAS!</p>
+                        <p>PROTEGEMOS TU PYME Y TE AYDUAMOS EN LOS<br/>PROCESOS DE COBRANZA<br/>SI NO RECUPERAS, ¡NO PAGAS!</p>
                         <img src={DoubleLine} alt="doubleline" className="doubleline py-5" />
-                        <h1>¡RECUPERA TU DINERO!</h1>
-                        <p>TIENES DERECHO A COBRAR JUDICIALMENTE LO</p>
-                        <p>QUE NO TE PAGARON. ¡PRUEBA CON NOSOTROS!</p>
-                        <img src={Button} alt="button" className="button" />
+                        <h1 className="mb-3">¡RECUPERA TU DINERO!</h1>
+                        <p>TIENES DERECHO A COBRAR JUDICIALMENTE LO<br/>QUE NO TE PAGARON. ¡PRUEBA CON NOSOTROS!</p>
+                        <img src={Button} alt="button" className="button mt-4" />
                     </div>
                 </div>
                 <div className="col-3 d-flex align-bottom">
