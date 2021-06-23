d2e7b2413b1bc93db2f5d05081022cb21decadaf
Jerónimo Santa María Illanes <jero@MacBook-Pro-de-Jeronimo.local>
2021-06-23 14:43:15 -0400

    adding a possible footer

--- front/src/components/footer.jsx
+++ front/src/components/footer.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = props => {
     return(
-        <div className="footer no-gutters">
+        <footer className="footer text-center py-4 no-gutters">
             <h1>Diseñado y Programado por JJWebStudio</h1>
-        </div>
+        </footer>
     );
 }
 
