bf8ed8da1ca0b2817d13728891a9a1e5579972cc
Jerónimo Santa María Illanes <jero@MacBook-Pro-de-Jeronimo.local>
2021-08-10 19:55:44 -0400

    adding precios

--- front/src/views/landsection/precios.jsx
+++ front/src/views/landsection/precios.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import Tabla from '../../img/tabla.svg';
-import PersonaDos from '../../img/personados.png';
+import Tabla from '../../img/tabla.png';
+import NuestrosPrecios from '../../img/nuestrosprecios.svg';
 import SignoPeso from '../../img/signopeso.svg';
+import Torta from '../../img/torta.svg';
+import Cotiza from '../../img/cotiza.svg';
 import DoubleLine from '../../img/doubleline.svg';
 
 const Precios = props => {
     return (
         <div className="precios-cont text-center no-gutters">
-            <h1>TRANSPARENCIA</h1>
-            <p>LO QUE NOS CARACTERIZA</p>
+            <h1 className="primtit">TRANSPARENCIA</h1>
+            <p className="subprimtit">LO QUE NOS CARACTERIZA</p>
             <div className="grupo d-flex justify-content-center no-gutters">
-                <img src={PersonaDos} alt="personados" className="personados" />
                 <img src={SignoPeso} alt="signopeso" className="signopeso" />
             </div>
-            <h1 className="py-5">NUESTROS PRECIOS</h1>
+            <img src={NuestrosPrecios} alt="nuestrosprecios" className="nuestrosprecios" />
             <img src={Tabla} alt="tabla" className="tabla" />
-            <div className="condiciones text-center pt-3">
-                <p className="mb-0">*No incluye costas del juicio</p>
-                <p className="mb-0 mt-n2">*Juicios ordinarios</p>
-                <p className="mb-0 mt-n2">*Desde 12UF</p>
+            <div className="condiciones text-center">
+                <p className="mb-0">* Los juicios NO comprenden gastos judiciales.</p>
+                <p className="mb-0 mt-n2">RECEPTORES y otros aplicables.</p>
+                <p className="mb-0 mt-n2">* El anticipo comprende solo primera instancia</p>
             </div>
-            <img src={DoubleLine} alt="doubleline" className="doubleline my-5 pt-4" />
+            <div className="juiord">
+                <h1 className="primtit">JUICIOS ORDINARIOS DE<br></br>COBRO DE PESOS</h1>
+                <p>DESDE $ 399.000 + % DE RECUPERO</p>
+            </div>
+            <img src={Torta} alt="torta" className="torta" />
+            <br/>
+            <img src={Cotiza} alt="cotiza" className="cotiza" />
+            <br/>
+            <img src={DoubleLine} alt="doubleline" className="doubleline" />
         </div>
     );
 }
