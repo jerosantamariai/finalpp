435edf30ad7bdc889ae3c25c02a9d9488933b1cc
Jerónimo Santa María Illanes <jero@MacBook-Pro-de-Jeronimo.local>
2021-08-10 13:43:47 -0400

    adding format img and quehacemos

--- front/src/views/home.jsx
+++ front/src/views/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Inicio from './landsection/inicio';
+import QueHacemos from './landsection/quehacemos';
 import Trabajamos from './landsection/trabajamos';
 import PorQue from './landsection/porque';
 import Precios from './landsection/precios';
@@ -9,6 +10,7 @@ const Home = props => {
     return(
         <div className="home no-gutters">
             <Inicio />
+            <QueHacemos />
             <Trabajamos />
             <PorQue />
             <Precios />
--- front/src/views/landsection/porque.jsx
+++ front/src/views/landsection/porque.jsx
@@ -3,14 +3,14 @@ import CuadroUno from '../../img/cuadrouno.svg';
 import CuadroDos from '../../img/cuadrodos.svg';
 import CuadroTres from '../../img/cuadrotres.svg';
 import CuadroCuatro from '../../img/cuadrocuatro.svg';
-import Rocket from '../../img/rocket.svg';
+// import Rocket from '../../img/rocket.svg';
 
 const PorQue = props => {
     return(
         <div className="porque-cont text-center my-5 no-gutters">
             <h1 className="mb-3">¿POR QUÉ TRABAJAR JUNTOS?</h1>
             <p>NUESTRO TRABAJO ES GARATÍA DE COMPROMISO, DEDICACIÓN Y EXPERIENCIA</p>
-            <img src={Rocket} alt="rocket" className="rocket" />
+            {/* <img src={Rocket} alt="rocket" className="rocket" /> */}
             <div className="cuadros d-flex justify-content-center py-5 no-gutters">
                 <img src={CuadroUno} alt="cuadrouno" className="cuadro" />
                 <img src={CuadroDos} alt="cuadrodos" className="cuadro" />
