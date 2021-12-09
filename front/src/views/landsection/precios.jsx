137442ce1cce5c2e49f5c05231f818f0cfc8e382
jerosantamariai <jerosantamariai@gmail.com>
2021-12-09 01:01:31 -0300

    precios ok

--- front/src/views/landsection/precios.jsx
+++ front/src/views/landsection/precios.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import Tabla from '../../img/tabla.png';
 import NuestrosPrecios from '../../img/nuestrosprecios.svg';
 import SignoPeso from '../../img/signopeso.svg';
-import Torta from '../../img/torta.svg';
+// import Torta from '../../img/torta.svg';
 import Cotiza from '../../img/cotiza.svg';
 import DoubleLine from '../../img/doubleline.svg';
+import LightBulb from '../../img/lightbulb.png';
 
 const Precios = props => {
     return (
@@ -15,18 +16,18 @@ const Precios = props => {
             <img src={NuestrosPrecios} alt="nuestrosprecios" className="nuestrosprecios" />
             <br/>
             <img src={Tabla} alt="tabla" className="tabla" />
-            <div className="condiciones text-center">
-                <p className="mb-0">* Los juicios NO comprenden gastos judiciales.</p>
-                <p className="mb-0 mt-n2">RECEPTORES y otros aplicables.</p>
-                <p className="mb-0 mt-n2">* El anticipo comprende solo primera instancia</p>
+            <div className="condiciones text-right">
+                <p className="condtext mb-2">* Los juicios NO comprenden gastos judiciales. RECEPTORES y otros aplicables.</p>
+                <p className="condtext mb-0 mt-n2">* El anticipo comprende solo primera instancia</p>
             </div>
             <div className="juiord">
+                <img src={LightBulb} alt="lightbulb" className="lightbulb" />
                 <h1 className="primtit">JUICIOS ORDINARIOS DE<br></br>COBRO DE PESOS</h1>
                 <p>DESDE $ 399.000 + % DE RECUPERO</p>
                 <br/>
                 <img src={Cotiza} alt="cotiza" className="cotiza" />
             </div>
-            <img src={Torta} alt="torta" className="torta" />
+            {/* <img src={Torta} alt="torta" className="torta" /> */}
             <br/>
             <img src={DoubleLine} alt="doubleline" className="doubleline" />
         </div>
