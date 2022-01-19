b14c1e214c20cec8efc5418616cfe5e5b1f5fa5a
jerosantamariai <jerosantamariai@gmail.com>
2022-01-18 23:26:02 -0300

    first steps on mobile version

--- front/src/views/home.jsx
+++ front/src/views/home.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import Inicio from './landsection/inicio';
-import QueHacemos from './landsection/quehacemos';
-import Trabajamos from './landsection/trabajamos';
-import PorQue from './landsection/porque';
-import Precios from './landsection/precios';
-import Hablemos from './landsection/hablemos';
+// import QueHacemos from './landsection/quehacemos';
+// import Trabajamos from './landsection/trabajamos';
+// import PorQue from './landsection/porque';
+// import Precios from './landsection/precios';
+// import Hablemos from './landsection/hablemos';
 
 const Home = props => {
     return(
         <div className="home no-gutters">
             <Inicio />
-            <QueHacemos />
-            <Trabajamos />
-            <PorQue />
-            <Precios />
-            <Hablemos />
+            {/* <QueHacemos /> */}
+            {/* <Trabajamos /> */}
+            {/* <PorQue /> */}
+            {/* <Precios /> */}
+            {/* <Hablemos /> */}
         </div>
     );
 }
