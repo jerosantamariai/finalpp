6ed4bb99cfeb124ad64a51bd9b83c7e9d0a7cf48
jerosantamariai <jerosantamariai@gmail.com>
2022-01-30 23:37:36 -0300

    hope last commit

--- front/src/components/form.jsx
+++ front/src/components/form.jsx
@@ -4,15 +4,15 @@ const Form = props => {
     return (
         <form method='post' action='sendmail.php' >
             <div className="mb-3">
-                <label for="exampleInputEmail" className="form-label">EMAIL</label>
+                <label className="form-label">EMAIL</label>
                 <input type="email" className="form-control" id="exampleInputEmail" aria-describedby="emailHelp" name='email' required />
             </div>
             <div className="mb-3">
-                <label for="exampleInputName" className="form-label">NOMBRE</label>
+                <label className="form-label">NOMBRE</label>
                 <input type="text" className="form-control" id="exampleInputName" name='name' required />
             </div>
             <div className="mb-3">
-                <label for="exampleInputComment" className="form-label">COMENTARIO</label>
+                <label className="form-label">COMENTARIO</label>
                 <textarea type="text" className="form-control" id="exampleInputComentario" name='comments' required />
             </div>
             {/* <img src={Submit} alt="submit" type="submit" className="submit mt-4" /> */}
